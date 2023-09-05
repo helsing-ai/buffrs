c6cfac8e52e2e7e41547226c1b4f2fc4251290be
Mara Schulke <mara.schulke@helsing.ai>
2023-09-05 10:52:24 +0200

    Make the `build` and `git` features to be opt out
    Reduces build times a lot!

--- src/generator.rs
+++ src/generator.rs
@@ -119,20 +119,3 @@ pub async fn generate(language: Language) -> eyre::Result<()> {
 
     Ok(())
 }
-
-/// Include generated rust language bindings for buffrs.
-///
-/// ```rust,ignore
-/// mod protos {
-///     buffrs::include!();
-/// }
-/// ```
-#[macro_export]
-macro_rules! include {
-    () => {
-        ::std::include!(concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/proto/build/rust/mod.rs",
-        ));
-    };
-}
