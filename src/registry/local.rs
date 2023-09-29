e4f3f92d29586b1e69f9ee0f2a909be55600ac4a
andre.mello <andre.mello@helsing.ai>
2023-09-29 14:32:34 +0100

    uncomment local registry module

--- src/registry/local.rs
+++ src/registry/local.rs
@@ -1,173 +1,186 @@
-// // Copyright 2023 Helsing GmbH
-// //
-// // Licensed under the Apache License, Version 2.0 (the "License");
-// // you may not use this file except in compliance with the License.
-// // You may obtain a copy of the License at
-// //
-// //     http://www.apache.org/licenses/LICENSE-2.0
-// //
-// // Unless required by applicable law or agreed to in writing, software
-// // distributed under the License is distributed on an "AS IS" BASIS,
-// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// // See the License for the specific language governing permissions and
-// // limitations under the License.
+// Copyright 2023 Helsing GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// use std::path::PathBuf;
+use std::path::PathBuf;
 
-// use bytes::Bytes;
-// use eyre::{ensure, ContextCompat};
+use bytes::Bytes;
+use eyre::{ensure, ContextCompat};
 
-// use crate::{manifest::Dependency, package::Package};
+use crate::{manifest::Dependency, package::Package};
 
-// use super::Registry;
+use super::{DownloadError, PublishError, Registry};
 
-// /// A registry that stores and retries packages from a local file system.
-// /// This registry is intended primarily for testing.
-// #[derive(Debug, Clone)]
-// pub struct LocalRegistry {
-//     base_dir: PathBuf,
-// }
+/// A registry that stores and retries packages from a local file system.
+/// This registry is intended primarily for testing.
+#[derive(Debug, Clone)]
+pub struct LocalRegistry {
+    base_dir: PathBuf,
+}
 
-// impl LocalRegistry {
-//     #[allow(dead_code)]
-//     pub fn new(base_dir: PathBuf) -> Self {
-//         LocalRegistry { base_dir }
-//     }
-// }
+impl LocalRegistry {
+    #[allow(dead_code)]
+    pub fn new(base_dir: PathBuf) -> Self {
+        LocalRegistry { base_dir }
+    }
+}
 
-// #[async_trait::async_trait]
-// impl Registry for LocalRegistry {
-//     async fn download(&self, dependency: Dependency) -> eyre::Result<Package> {
-//         // TODO(rfink): Factor out checks so that artifactory and local registry both use them
-//         ensure!(
-//             dependency.manifest.version.comparators.len() == 1,
-//             "{} uses unsupported semver comparators",
-//             dependency.package
-//         );
+#[async_trait::async_trait]
+impl Registry for LocalRegistry {
+    async fn download(&self, dependency: Dependency) -> Result<Package, DownloadError> {
+        // TODO(rfink): Factor out checks so that artifactory and local registry both use them
+        if dependency.manifest.version.comparators.len() != 1 {
+            return Err(DownloadError::UnsupportedVersionRequirement(
+                dependency.manifest.version,
+            ));
+        }
 
-//         let version = dependency
-//             .manifest
-//             .version
-//             .comparators
-//             .first()
-//             .wrap_err("internal error")?;
+        let version = dependency
+            .manifest
+            .version
+            .comparators
+            .first()
+            // validated above
+            .expect("Unexpected error: empty comparators vector in VersionReq");
 
-//         ensure!(
-//             version.op == semver::Op::Exact && version.minor.is_some() && version.patch.is_some(),
-//             "local registry only support pinned dependencies"
-//         );
+        if version.op != semver::Op::Exact || version.minor.is_none() || version.patch.is_none() {
+            return Err(DownloadError::UnsupportedVersionRequirement(
+                dependency.manifest.version,
+            ));
+        }
 
-//         let version = format!(
-//             "{}.{}.{}{}",
-//             version.major,
-//             version.minor.wrap_err("internal error")?,
-//             version.patch.wrap_err("internal error")?,
-//             if version.pre.is_empty() {
-//                 "".to_owned()
-//             } else {
-//                 format!("-{}", version.pre)
-//             }
-//         );
+        let version = format!(
+            "{}.{}.{}{}",
+            version.major,
+            version
+                .minor
+                .expect("Unexpected error: minor number missing"), // validated above
+            version
+                .patch
+                .expect("Unexpected error: patch number missing"), // validated above
+            if version.pre.is_empty() {
+                "".to_owned()
+            } else {
+                format!("-{}", version.pre)
+            }
+        );
 
-//         let path = self.base_dir.join(PathBuf::from(format!(
-//             "{}/{}/{}-{}.tgz",
-//             dependency.manifest.repository, dependency.package, dependency.package, version
-//         )));
+        let path = self.base_dir.join(PathBuf::from(format!(
+            "{}/{}/{}-{}.tgz",
+            dependency.manifest.repository, dependency.package, dependency.package, version
+        )));
 
-//         tracing::debug!("downloaded dependency {dependency} from {:?}", path);
+        tracing::debug!("downloaded dependency {dependency} from {:?}", path);
 
-//         let bytes = Bytes::from(std::fs::read(path)?);
-//         Package::try_from(bytes)
-//     }
+        let bytes = Bytes::from(
+            std::fs::read(path)
+                .map_err(|_| DownloadError::RequestFailed("Could not read file".into()))?,
+        );
 
-//     async fn publish(&self, package: Package, repository: String) -> eyre::Result<()> {
-//         let path = self.base_dir.join(PathBuf::from(format!(
-//             "{}/{}/{}-{}.tgz",
-//             repository,
-//             package.name(),
-//             package.name(),
-//             package.version(),
-//         )));
+        Package::try_from(bytes)
+            .map_err(|_| DownloadError::InvalidResponse("failed to decode tarball".into()))
+    }
 
-//         std::fs::create_dir_all(path.parent().unwrap()).unwrap();
+    async fn publish(&self, package: Package, repository: String) -> Result<(), PublishError> {
+        let path = self.base_dir.join(PathBuf::from(format!(
+            "{}/{}/{}-{}.tgz",
+            repository,
+            package.name(),
+            package.name(),
+            package.version(),
+        )));
 
-//         std::fs::write(&path, &package.tgz)?;
+        std::fs::create_dir_all(path.parent().unwrap()).unwrap();
 
-//         tracing::info!(
-//             ":: published {}/{}@{} to {:?}",
-//             repository,
-//             package.name(),
-//             package.version(),
-//             path
-//         );
+        std::fs::write(&path, &package.tgz).map_err(|err| {
+            PublishError::RequestFailed(format!("Could not write to file: {err}"))
+        })?;
 
-//         Ok(())
-//     }
-// }
+        tracing::info!(
+            ":: published {}/{}@{} to {:?}",
+            repository,
+            package.name(),
+            package.version(),
+            path
+        );
 
-// #[cfg(test)]
-// mod tests {
-//     use crate::manifest::{Dependency, Manifest, PackageManifest};
-//     use crate::package::{Package, PackageType};
-//     use crate::registry::local::LocalRegistry;
-//     use crate::registry::{Registry, RegistryUri};
-//     use bytes::Bytes;
-//     use std::path::PathBuf;
-//     use std::str::FromStr;
-//     use std::{env, fs};
+        Ok(())
+    }
+}
 
-//     #[tokio::test]
-//     #[ignore]
-//     async fn can_publish_and_fetch() {
-//         let dir = env::temp_dir();
-//         let registry = LocalRegistry::new(dir.clone());
+#[cfg(test)]
+mod tests {
+    use crate::manifest::{Dependency, Manifest, PackageManifest};
+    use crate::package::{Package, PackageType};
+    use crate::registry::local::LocalRegistry;
+    use crate::registry::{Registry, RegistryUri};
+    use bytes::Bytes;
+    use std::path::PathBuf;
+    use std::str::FromStr;
+    use std::{env, fs};
 
-//         let manifest = Manifest {
-//             package: PackageManifest {
-//                 kind: PackageType::Api,
-//                 name: "test-api".parse().unwrap(),
-//                 version: "0.1.0".parse().unwrap(),
-//                 description: None,
-//             },
-//             dependencies: vec![],
-//         };
+    #[tokio::test]
+    #[ignore]
+    async fn can_publish_and_fetch() {
+        let dir = env::temp_dir();
+        let registry = LocalRegistry::new(dir.clone());
 
-//         // let package_bytes =
-//         //     Bytes::from(include_bytes!("../../tests/data/packages/test-api-0.1.0.tgz").to_vec());
-//         let package_bytes = Bytes::new();
+        let manifest = Manifest {
+            package: PackageManifest {
+                kind: PackageType::Api,
+                name: "test-api".parse().unwrap(),
+                version: "0.1.0".parse().unwrap(),
+                description: None,
+            },
+            dependencies: vec![],
+        };
 
-//         // Publish to local registry and assert the tgz exists in the file system
-//         registry
-//             .publish(
-//                 Package::new(manifest.clone(), package_bytes.clone()),
-//                 "test-repo".into(),
-//             )
-//             .await
-//             .unwrap();
+        // let package_bytes =
+        //     Bytes::from(include_bytes!("../../tests/data/packages/test-api-0.1.0.tgz").to_vec());
+        let package_bytes = Bytes::new();
 
-//         assert_eq!(
-//             Bytes::from(
-//                 fs::read(dir.join(PathBuf::from("test-repo/test-api/test-api-0.1.0.tgz"))).unwrap()
-//             ),
-//             package_bytes
-//         );
+        // Publish to local registry and assert the tgz exists in the file system
+        registry
+            .publish(
+                Package::new(manifest.clone(), package_bytes.clone()),
+                "test-repo".into(),
+            )
+            .await
+            .unwrap();
 
-//         let registry_uri = RegistryUri::from_str("http://some-registry/artifactory")
-//             .expect("Failed to parse registry URL");
+        assert_eq!(
+            Bytes::from(
+                fs::read(dir.join(PathBuf::from("test-repo/test-api/test-api-0.1.0.tgz"))).unwrap()
+            ),
+            package_bytes
+        );
 
-//         // Download package from local registry and assert the tgz bytes and the metadata match what we
-//         // had published.
-//         let fetched = registry
-//             .download(Dependency::new(
-//                 registry_uri,
-//                 "test-repo".into(),
-//                 "test-api".parse().unwrap(),
-//                 "=0.1.0".parse().unwrap(),
-//             ))
-//             .await
-//             .unwrap();
+        let registry_uri = RegistryUri::from_str("http://some-registry/artifactory")
+            .expect("Failed to parse registry URL");
 
-//         assert_eq!(fetched.manifest, manifest);
-//         assert_eq!(fetched.tgz, package_bytes);
-//     }
-// }
+        // Download package from local registry and assert the tgz bytes and the metadata match what we
+        // had published.
+        let fetched = registry
+            .download(Dependency::new(
+                registry_uri,
+                "test-repo".into(),
+                "test-api".parse().unwrap(),
+                "=0.1.0".parse().unwrap(),
+            ))
+            .await
+            .unwrap();
+
+        assert_eq!(fetched.manifest, manifest);
+        assert_eq!(fetched.tgz, package_bytes);
+    }
+}
