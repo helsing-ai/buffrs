e84625e6f880d180c0b0693343b98253ddbb4925
Patrick Elsen <patrick.elsen@helsing.ai>
2023-10-16 10:35:28 +0200

    Adds the proper license comment to every registry file.

--- registry/src/main.rs
+++ registry/src/main.rs
@@ -1,11 +1,24 @@
-// (c) Copyright 2023 Helsing GmbH. All rights reserved.
-use buffrs_registry::{db::connect, config::Config};
+// Copyright 2023 Helsing GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use buffrs_registry::{db::connect, options::Options};
 use clap::Parser;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    let config = Config::parse();
+    let config = Options::parse();
     tracing_subscriber::fmt::init();
-    let db = connect(&config.database).await.unwrap();
+    let db = connect(&config.database.as_str()).await.unwrap();
     Ok(())
 }
