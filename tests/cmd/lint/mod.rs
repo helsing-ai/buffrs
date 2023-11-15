a5611c4ac5a3ffc5a3b036f3c8edaaa66f449bec
Johannes Hutter <johannes.hutter@helsing.ai>
2023-11-15 18:24:52 +0100

    Add regression tests for buffrs lint

---
+++ tests/cmd/lint/mod.rs
@@ -0,0 +1,15 @@
+use crate::VirtualFileSystem;
+
+#[test]
+fn fixture() {
+    let vfs = VirtualFileSystem::copy(crate::parent_directory!().join("in"));
+
+    crate::cli!()
+        .arg("lint")
+        .current_dir(vfs.root())
+        .assert()
+        .success()
+        .stdout(include_str!("stdout.log"))
+        .stderr(include_str!("stderr.log"))
+        .code(0);
+}
