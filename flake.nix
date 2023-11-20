eda45848a84fe30a6adcdd4f62dc1df68acf7478
james.baker@helsing.ai <james.baker@helsing.ai>
2023-11-20 16:22:09 +0000

    simplify

--- flake.nix
+++ flake.nix
@@ -13,23 +13,24 @@
         };
         naersk' = pkgs.callPackage naersk {};
         nativeBuildInputs = with pkgs; [ pkg-config ];
-      in rec {
-        # A function for using buffrs inside Nix projects.
-        # this will vendor dependencies into a Nix store path and return an attribute set which can be merged with 
-        # Rust derivations.
-        lib.vendorDependencies = pkgs.callPackage ./downloadBuffrsDependencies.nix { buffrs = packages.default; };
-        packages.default = naersk'.buildPackage {
+        package = naersk'.buildPackage {
           inherit nativeBuildInputs;
           src = ./.;
           buildInputs = with pkgs; [ openssl ] ++ (pkgs.lib.lists.optionals (stdenv.isDarwin) [ darwin.apple_sdk.frameworks.SystemConfiguration ]);
         };
+      in {
+        # A function for using buffrs inside Nix projects.
+        # this will vendor dependencies into a Nix store path and return an attribute set which can be merged with 
+        # Rust derivations.
+        lib.vendorDependencies = pkgs.callPackage ./downloadBuffrsDependencies.nix { buffrs = package; };
+        packages.default = package;
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; nativeBuildInputs ++ [
             cargo
             rustc
           ];
         };
-        checks.builds = packages.default;
+        checks.builds = package;
       }
     );
 }
