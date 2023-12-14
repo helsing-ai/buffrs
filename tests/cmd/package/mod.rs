137bb9229edef9bc8b047f1fca471826003c09de
Quentin Santos <quentin.santos@helsing.ai>
2023-12-14 17:10:07 +0100

    Unignore cmd::package
    git LFS was the likely culprit and is now disabled

--- tests/cmd/package/mod.rs
+++ tests/cmd/package/mod.rs
@@ -1,7 +1,6 @@
 use crate::VirtualFileSystem;
 
 #[test]
-#[ignore = "tgz header issue / fs mismatch"]
 fn fixture() {
     let vfs = VirtualFileSystem::copy(crate::parent_directory!().join("in"));
 
