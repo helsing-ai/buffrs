02a8031dfbabe257773855b2ef446963e64dcf23
BlowaXD <16936368+BlowaXD@users.noreply.github.com>
2024-01-10 15:52:05 +0000

    move test to use the server implementation directly

--- registry/tests/api/main.rs
+++ registry/tests/api/main.rs
@@ -1,6 +1,13 @@
+use buffrs_registry::context::Context;
+use buffrs_registry::metadata::memory::InMemoryMetadataStorage;
 use buffrs_registry::proto::buffrs::package::{Compressed, Package};
-use buffrs_registry::proto::buffrs::registry::registry_client::RegistryClient;
+
+use buffrs_registry::proto::buffrs::registry::registry_server::Registry;
 use buffrs_registry::proto::buffrs::registry::PublishRequest;
+use buffrs_registry::storage;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::path::Path;
+use std::sync::Arc;
 use tonic::Code;
 
 pub fn create_publish_request_sample() -> PublishRequest {
@@ -16,15 +23,18 @@ pub fn create_publish_request_sample() -> PublishRequest {
     }
 }
 
-#[ignore = "temporary"]
 #[tokio::test]
 async fn test_publish_registry() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = RegistryClient::connect("http://localhost:4367").await?;
+    let path = Path::new("/tmp");
+    let storage = Arc::new(storage::Filesystem::new(path));
+    let metadata = Arc::new(InMemoryMetadataStorage::new());
+    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 0)), 1111);
+    let fake_server = Context::new(storage, metadata, socket);
 
     // 1. Insert a package and expect it to be successful
     {
         let req = tonic::Request::new(create_publish_request_sample());
-        let _res = client.publish(req).await?;
+        let _res = fake_server.publish(req).await?;
         println!(":: Package Publish OK");
     }
 
@@ -32,7 +42,11 @@ async fn test_publish_registry() -> Result<(), Box<dyn std::error::Error>> {
     {
         // duplicate check
         let req = tonic::Request::new(create_publish_request_sample());
-        let Err(res) = client.publish(req).await.map_err(|status| status.code()) else {
+        let Err(res) = fake_server
+            .publish(req)
+            .await
+            .map_err(|status| status.code())
+        else {
             panic!("Publish duplicate")
         };
         assert_eq!(res, Code::AlreadyExists);
