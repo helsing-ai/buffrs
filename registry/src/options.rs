da1743a989d103802fb7e68eea133b66127c1ae2
BlowaXD <16936368+BlowaXD@users.noreply.github.com>
2024-01-10 15:16:47 +0000

    Add registry Publish with memory metadata database (temporary)

--- registry/src/options.rs
+++ registry/src/options.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use buffrs_registry::metadata::new_storage;
 use buffrs_registry::{context::Context, storage::*};
 use clap::{Parser, ValueEnum};
 use eyre::Result;
@@ -31,7 +32,8 @@ pub struct Options {
 impl Options {
     pub async fn build(&self) -> Result<Context> {
         let storage = self.storage.build().await?;
-        Ok(Context::new(storage))
+        let metadata = new_storage();
+        Ok(Context::new(storage, metadata, self.listen))
     }
 }
 
---
+++ registry/tests/api/main.rs
@@ -0,0 +1,42 @@
+use buffrs_registry::proto::buffrs::package::{Compressed, Package};
+use buffrs_registry::proto::buffrs::registry::registry_client::RegistryClient;
+use buffrs_registry::proto::buffrs::registry::PublishRequest;
+use tonic::Code;
+
+pub fn create_publish_request_sample() -> PublishRequest {
+    PublishRequest {
+        package: Some(Compressed {
+            metadata: Some(Package {
+                name: "testing".to_string(),
+                version: "1.0.0".to_string(),
+                r#type: 0,
+            }),
+            tgz: vec![0, 0, 0],
+        }),
+    }
+}
+
+#[tokio::test]
+async fn test_publish_registry() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = RegistryClient::connect("http://localhost:4367").await?;
+
+    // 1. Insert a package and expect it to be successful
+    {
+        let req = tonic::Request::new(create_publish_request_sample());
+        let _res = client.publish(req).await?;
+        println!(":: Package Publish OK");
+    }
+
+    // 2. Insert the same package for a duplicate check
+    {
+        // duplicate check
+        let req = tonic::Request::new(create_publish_request_sample());
+        let Err(res) = client.publish(req).await.map_err(|status| status.code()) else {
+            panic!("Publish duplicate")
+        };
+        assert_eq!(res, Code::AlreadyExists);
+        println!(":: Package Forbid Duplicate OK");
+    }
+
+    Ok(())
+}
