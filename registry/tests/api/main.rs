afb820053e3c6a5993b0b457d555b571095474fc
BlowaXD <16936368+BlowaXD@users.noreply.github.com>
2024-01-11 10:59:49 +0000

    cleanup service testing

--- registry/tests/api/main.rs
+++ registry/tests/api/main.rs
@@ -1,14 +1,23 @@
 use buffrs_registry::context::Context;
 use buffrs_registry::metadata::memory::InMemoryMetadataStorage;
 use buffrs_registry::proto::buffrs::package::{Compressed, Package};
-
-use buffrs_registry::proto::buffrs::registry::registry_server::Registry;
+use buffrs_registry::proto::buffrs::registry::registry_client::RegistryClient;
+use buffrs_registry::proto::buffrs::registry::registry_server::{Registry, RegistryServer};
 use buffrs_registry::proto::buffrs::registry::PublishRequest;
 use buffrs_registry::storage;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::path::Path;
+use std::str::FromStr;
 use std::sync::Arc;
+use tokio::io::DuplexStream;
+use tonic::codegen::tokio_stream;
+use tonic::transport::{Server, Channel};
 use tonic::Code;
+use tonic::{
+    transport::{Endpoint, Uri},
+    Request, Response, Status,
+};
+use tower::service_fn;
 
 pub fn create_publish_request_sample() -> PublishRequest {
     PublishRequest {
@@ -23,18 +32,58 @@ pub fn create_publish_request_sample() -> PublishRequest {
     }
 }
 
-#[tokio::test]
-async fn test_publish_registry() -> Result<(), Box<dyn std::error::Error>> {
+pub async fn basic_setup() -> Result<RegistryClient<Channel>, Box<dyn std::error::Error>>
+{
+    let (client, server) = tokio::io::duplex(1024);
+
     let path = Path::new("/tmp");
     let storage = Arc::new(storage::Filesystem::new(path));
     let metadata = Arc::new(InMemoryMetadataStorage::new());
-    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 0)), 1111);
-    let fake_server = Context::new(storage, metadata, socket);
+
+    let url = "0.0.0.0:0";
+
+    // this is needs to be removed once Context got cleaned up
+    let socket = SocketAddr::from_str(url).expect("Shouldn't happen");
+    let context = Context::new(storage, metadata, socket);
+
+    let _handle = tokio::spawn(async move {
+        Server::builder()
+            .add_service(RegistryServer::new(context))
+            .serve_with_incoming(tokio_stream::once(Ok::<_, std::io::Error>(server)))
+            .await
+    });
+
+    // Move client to an option so we can _move_ the inner value
+    // on the first attempt to connect. All other attempts will fail.
+    let mut client = Some(client);
+    let channel = Endpoint::try_from(format!("http://{}", url))?
+        .connect_with_connector(service_fn(move |_: Uri| {
+            let client = client.take();
+
+            async move {
+                if let Some(client) = client {
+                    Ok(client)
+                } else {
+                    Err(std::io::Error::new(
+                        std::io::ErrorKind::Other,
+                        "Client already taken",
+                    ))
+                }
+            }
+        }))
+        .await?;
+
+    return Ok(RegistryClient::new(channel));
+}
+
+#[tokio::test]
+async fn test_publish_registry() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = basic_setup().await?;
 
     // 1. Insert a package and expect it to be successful
     {
         let req = tonic::Request::new(create_publish_request_sample());
-        let _res = fake_server.publish(req).await?;
+        let _res = client.publish(req).await?;
         println!(":: Package Publish OK");
     }
 
@@ -42,11 +91,7 @@ async fn test_publish_registry() -> Result<(), Box<dyn std::error::Error>> {
     {
         // duplicate check
         let req = tonic::Request::new(create_publish_request_sample());
-        let Err(res) = fake_server
-            .publish(req)
-            .await
-            .map_err(|status| status.code())
-        else {
+        let Err(res) = client.publish(req).await.map_err(|status| status.code()) else {
             panic!("Publish duplicate")
         };
         assert_eq!(res, Code::AlreadyExists);
