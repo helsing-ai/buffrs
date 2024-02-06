e0a592ce1608320b5262af67b20c415f140c6389
edward.gokmen <edward.gokmen@helsing.ai>
2024-02-06 13:53:01 +0000

    can now build buffrs

--- flake.nix
+++ flake.nix
@@ -11,23 +11,31 @@
         pkgs = (import nixpkgs) {
           inherit system;
         };
+        inherit (pkgs) lib;
+
         naersk' = pkgs.callPackage naersk {};
         nativeBuildInputs = with pkgs; [ pkg-config ];
+
+        darwin_frameworks = with pkgs.darwin.apple_sdk.frameworks; [
+          Security
+          SystemConfiguration
+        ];
       in rec {
         packages.default = naersk'.buildPackage {
           inherit nativeBuildInputs;
           src = ./.;
-          buildInputs = with pkgs; [ openssl perl ] ++ (pkgs.lib.lists.optionals (stdenv.isDarwin) [ darwin.apple_sdk.frameworks.SystemConfiguration ]);
+          buildInputs = with pkgs; [ openssl perl ] ++ lib.lists.optionals stdenv.isDarwin darwin_frameworks;
 
           OPENSSL_NO_VENDOR = 1;
         };
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; nativeBuildInputs ++ [
+          LIBGIT2_NO_VENDOR = 1;
+          buildInputs = with pkgs; nativeBuildInputs ++ [
             cargo
             libgit2
             libiconv
             rustc
-          ];
+          ] ++ lib.lists.optionals stdenv.isDarwin darwin_frameworks;
         };
         checks.builds = packages.default;
       }
