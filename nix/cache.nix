c343f3b28d54a07158305ed437ded2c24c87574e
Mara Schulke <mara.schulke@helsing.ai>
2024-02-21 13:58:24 +0100

    Reformat cache.nix

--- nix/cache.nix
+++ nix/cache.nix
@@ -1,7 +1,8 @@
-{ fetchurl, runCommand, lib, buffrs, symlinkJoin }: lockfile:
+{ fetchurl, runCommand, lib, buffrs, symlinkJoin }:
+lockfile:
 
 let
-  src = runCommand "vendor-lockfile" {} ''
+  src = runCommand "vendor-lockfile" { } ''
     mkdir -p $out
     cp ${lockfile} $out/Proto.lock
   '';
@@ -14,21 +15,23 @@ let
 
   fileRequirements = builtins.fromJSON (builtins.readFile fileRequirementsJson);
 
-  fetchPackages = (file: let
-    package = file.package;
-    prefix = "sha256:";
-    sha256 = with lib; with file;
-      assert strings.hasPrefix prefix digest;
-      strings.removePrefix prefix digest;
+  fetchPackages = (file:
+    let
+      package = file.package;
+      prefix = "sha256:";
+      sha256 = with lib;
+        with file;
+        assert strings.hasPrefix prefix digest;
+        strings.removePrefix prefix digest;
 
-    tar = fetchurl {
-      inherit sha256;
-      url = file.url;
-    };
-  in runCommand "extract" {} ''
-    mkdir -p $out
-    cp ${tar} $out/${package}.sha256.${tar}.tgz
-  '');
+      tar = fetchurl {
+        inherit sha256;
+        url = file.url;
+      };
+    in runCommand "extract" { } ''
+      mkdir -p $out
+      cp ${tar} $out/${package}.sha256.${tar}.tgz
+    '');
 
   allPackages = map fetchPackages fileRequirements;
 in {
