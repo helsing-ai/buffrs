d55b9204313bb917384e0668e1e0ccaaaa09cf32
Alexis Bourget <alexis.bourget@helsing.ai>
2024-04-26 10:28:29 +0200

    chore: remove registry crate, it's not the approach we want to take

--- registry/migrations/20230828111844_packages.down.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE packages_invites;
-DROP TABLE package_owners;
-DROP TABLE packages;
-DROP TYPE package_type;
--- registry/ui/next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-}
-
-export default nextConfig
