5c3a66426167b0d957393966178ec204aa24be12
Mara Schulke <mara.schulke@helsing.ai>
2026-01-26 14:02:01 +0100

    Debug code

--- nix/cache.nix
+++ nix/cache.nix
@@ -1,4 +1,10 @@
-{ fetchurl, runCommand, lib, buffrs, symlinkJoin }:
+{
+  fetchurl,
+  runCommand,
+  lib,
+  buffrs,
+  symlinkJoin,
+}:
 
 lockfile:
 
@@ -8,32 +14,37 @@ let
     cp ${lockfile} $out/Proto.lock
   '';
 
-  fileRequirementsJson =
-    runCommand "buffrs-urls" { buildInputs = [ buffrs ]; } ''
-      cd ${src}
-      buffrs lock print-files > $out
-    '';
+  fileRequirementsJson = runCommand "buffrs-urls" { buildInputs = [ buffrs ]; } ''
+    cd ${src}
+    buffrs lock print-files
+    buffrs lock print-files > $out
+  '';
 
   fileRequirements = builtins.fromJSON (builtins.readFile fileRequirementsJson);
 
-  cachePackage = (file:
+  cachePackage = (
+    file:
     let
       prefix = "sha256:";
 
-      sha256 = assert lib.strings.hasPrefix prefix file.digest;
+      sha256 =
+        assert lib.strings.hasPrefix prefix file.digest;
         lib.strings.removePrefix prefix file.digest;
 
       tar = fetchurl {
         inherit sha256;
         url = file.url;
       };
-    in runCommand "cache-${file.package}" { } ''
+    in
+    runCommand "cache-${file.package}" { } ''
       mkdir -p $out
       cp ${tar} $out/${file.package}.sha256.${sha256}.tgz
-    '');
+    ''
+  );
 
   cache = map cachePackage fileRequirements;
-in {
+in
+{
   BUFFRS_CACHE = symlinkJoin {
     name = "buffrs-cache";
     paths = cache;
